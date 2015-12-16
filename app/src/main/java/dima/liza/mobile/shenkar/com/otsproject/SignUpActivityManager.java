aec8d75ccd4944724c6534c35aef3fd8a50f8a0b
DimaGirya <DimaGirya1990@gmail.com>
2015-12-16 19:31:49 +0200

    Intent

--- app/src/main/java/dima/liza/mobile/shenkar/com/otsproject/SignUpActivityManager.java
+++ app/src/main/java/dima/liza/mobile/shenkar/com/otsproject/SignUpActivityManager.java
@@ -1,6 +1,7 @@
 package dima.liza.mobile.shenkar.com.otsproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -60,6 +61,7 @@ public class SignUpActivityManager extends AppCompatActivity {
     }
 
     public void signUpManagerClicked(View view) {
-
+        Intent intent = new Intent (this,SignInActivityManager.class);
+        startActivity(intent);
     }
 }
